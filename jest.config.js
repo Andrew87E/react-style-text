0d304ae6f797ec9293faff86b08683fa2503303a
Andrew Edwards <aedwards@tifco.com>
2023-12-21 11:36:54 -0600

    updated jest

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+  testEnvironment: "jsdom",
+  verbose: true,
+};
+exports.default = config;
--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-import type { Config } from 'jest';
-
-const config: Config = {
-    testEnvironment: 'jsdom',
-    verbose: true,
-};
-
-export default config;
