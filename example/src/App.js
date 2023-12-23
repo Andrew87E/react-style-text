3be0a2e698755b3fba58fa6767940363054be224
Andrew Edwards <aedwards@tifco.com>
2023-12-23 14:23:56 -0600

    0.1.0 is stable and everything but the typewriter is working. typewriter for 0.2.0

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
-import { BrowserRouter as Router, Routes, Route, createBrowserRouter } from "react-router-dom";
 import { GlobalStyles } from './globalStyles';
 import theme from './theme';
 import Header from './components/Header/header';
 import Playground from './components/Playground/playground';
 import Footer from './components/Footer/footer';
-import ExamplePage from './components/ExamplePage/examplePage';
-
 
 function App() {
   return (
@@ -15,7 +12,7 @@ function App() {
       <GlobalStyles />
       <ThemeProvider theme={theme}>
         <Header />
-          <Playground />        
+        <Playground />        
         <Footer />
       </ThemeProvider>
     </>
